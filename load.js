ee166085a58e1b9b498e741488fce998bf69252a
clorofolle <ray@emailsnail.net>
2024-05-17 01:40:45 +0200

    fixed more urls to be relative

--- load.js
+++ load.js
@@ -1,21 +1,30 @@
 // Loads header, sidebars, footer. Paths to .html files are full URLs to ensure compatibility with github pages - oterwise, just keeping the URLs absolute will allow this code to be ran from any page on other hosts.
 
 document.addEventListener("DOMContentLoaded", function() {
-      loadSidebarContent('header', 'https://ray-of-sunshine.net/header.html');
-      loadSidebarContent('sidebar-left', 'https://ray-of-sunshine.net/sidebar-left.html');
-      loadSidebarContent('sidebar-right', 'https://ray-of-sunshine.net/sidebar-right.html');
-      loadSidebarContent('footer', 'https://ray-of-sunshine.net/footer.html');
-  });
-  
-  function loadSidebarContent(elementId, filePath, callback) {
+    var basePath = window.location.hostname === 'clorofolle.github.io' ? '/ray-of-sunshine.net/' : '/';
+    
+    loadSidebarContent('header', basePath + 'header.html');
+    loadSidebarContent('sidebar-left', basePath + 'sidebar-left.html');
+    loadSidebarContent('sidebar-right', basePath + 'sidebar-right.html');
+    loadSidebarContent('footer', basePath + 'footer.html');
+});
+
+function loadSidebarContent(elementId, filePath) {
     fetch(filePath)
-        .then(response => response.text())
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+            return response.text();
+        })
         .then(data => {
             document.getElementById(elementId).innerHTML = data;
-            if (callback) callback();
         })
-        .catch(err => console.error(err));
+        .catch(err => {
+            console.error(`Failed to load ${filePath}:`, err);
+        });
 }
+
 
 
 
@@ -23,10 +32,26 @@ document.addEventListener("DOMContentLoaded", function() {
 
 function toggleLanguage() {
     var elements = document.querySelectorAll('.eng, .ita');
+    var isEnglishVisible = document.querySelector('.eng:not(.hidden)') !== null;
+    var htmlElement = document.documentElement;
+
     elements.forEach(function(el) {
         el.classList.toggle('hidden');
     });
+
+    if (isEnglishVisible) {
+        // Switch to Italian
+        htmlElement.lang = 'it';
+    } else {
+        // Switch to English
+        htmlElement.lang = 'en';
+    }
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+    var button = document.getElementById('languageToggle');
+    button.addEventListener('click', toggleLanguage);
+});
 
 
 
